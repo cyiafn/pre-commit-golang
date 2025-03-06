d9491f6b292fa317974628b97d82c49342b4c26c
Chen Yifan <dev@cyifan.dev>
2025-03-06 11:18:14 +0800

    Update cmd-pkg.bash

--- lib/cmd-pkg.bash
+++ lib/cmd-pkg.bash
@@ -5,7 +5,6 @@
 
 prepare_file_hook_cmd "$@"
 
-export GO111MODULE=off
 error_code=0
 for sub in $(printf "%q\n" "${FILES[@]}" | xargs -n1 dirname | sort -u); do
 	if [ "${error_on_output:-}" -eq 1 ]; then
